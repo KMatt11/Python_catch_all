5136a5abe18d1f253150872222ea73e121d2ab4a
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-06-22 19:41:52 +0000

    Print Commit

--- variations.py
+++ variations.py
@@ -2,4 +2,16 @@
 
 # Python implementation here
 
-message = input("Enter a message")
+message = input("Enter a message: ")
+
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
+
+words = message.split()
+print("Words:", words)
+
+sorted_words = sorted(words)
+print("Alphabetic First Word:", sorted_words[0])
+print("Alphabetic Last Word:", sorted_words[-1])
