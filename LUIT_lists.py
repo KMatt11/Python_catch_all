1b1be14f5ffd1300f5894d6126ffca42f16c450f
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-06-23 21:01:42 +0000

    Final Add of Objective

--- LUIT_lists.py
+++ LUIT_lists.py
@@ -23,3 +23,10 @@ services.append("CloudFormation")
 # Objective 3 - Print the list and the length of the list
 print(services)
 
+# Objective 4 - Remove two specific services from the list by name or by index
+services.remove("S3")
+services.remove("CloudFormation")
+print(services)
+
+# Objective 5 - Print the new list and the new length of the list
+print (len(services))
