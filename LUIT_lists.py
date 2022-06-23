6ed0027ff983aa15ee5d1bebcc8437461c46ee02
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-06-23 20:43:26 +0000

    Added Python Services

--- LUIT_lists.py
+++ LUIT_lists.py
@@ -1,3 +1,25 @@
-"""
-Your module description
-"""
+#!/user/bin/env python3.8
+
+# Week 12 LUIT project: Create a list of services using Python
+
+# Objectives: 
+# Create a list of services using Python. IE: S3, Lambda, EC2, etc.
+# 1. The list should be empty initially.
+# 2. Populate the list using append or insert.
+# 3. Print the list and the length of the list.
+# 4. Remove two specific services from the list by name or by index.
+# 5. Print the new list and the new length of the list.
+
+# Objective 1 - Create an empty list
+services = []
+
+# Objective 2 - Populate the list using append or insert
+services.append("S3")
+services.append("Lamba")
+services.append("EC2")
+services.append("DynamoDB")
+services.append("CloudFormation")
+
+# Objective 3 - Print the list and the length of the list
+print(services)
+
