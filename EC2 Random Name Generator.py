57e4908ad37687dd7a0c21f31757ff211bc0efa4
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-06-30 15:59:54 +0000

    2nd commit

--- EC2 Random Name Generator.py
+++ EC2 Random Name Generator.py
@@ -1,6 +1,14 @@
-EC2 Random Name Generator
+#!/user/bin/env python3.8
 
-Several departments share an AWS environment. You need to ensure that the EC2s are properly named and are unique so team members can easily tell who the EC2 instances belong to. Use Python to create your unique EC2 names that the users can then attach to the instances. The Python Script should:
-1. Allow the user to input how many EC2 instances they want names for and output the same amount of unique names.
-2. Allow the user to input the name of their department that is used in the unique name.
-3. Generate random characters and numbers that will be included in the unique name.
+# Week 13 LUIT project: EC2 Random Name Generator
+
+# Objective: 
+# Several departments share an AWS environment. You need to ensure that the EC2s are properly named and are
+# unique so team members can easily tell who the EC2 instances belong to. Use Python to create your unique EC2 
+# names, so that the users can then attach to the instances. 
+
+#The Python Script should:
+
+# 1. Allow the user to input how many EC2 instances they want names for and output the same amount of unique names.
+# 2. Allow the user to input the name of their department that is used in the unique name.
+# 3. Generate random characters and numbers that will be included in the unique name.
